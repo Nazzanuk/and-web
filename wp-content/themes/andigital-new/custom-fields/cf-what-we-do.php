7ab0ee8b2b28812c7197714fccad9fc75be284fb
Nathan N <nazzanuk@gmail.com>
2015-03-21 01:03:34 +0000

    testing aws

--- wp-content/themes/andigital-new/custom-fields/cf-what-we-do.php
+++ wp-content/themes/andigital-new/custom-fields/cf-what-we-do.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 if (function_exists("register_field_group")) {
     register_field_group(array(
--- wp-content/themes/andigital-new/functions.php
+++ wp-content/themes/andigital-new/functions.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 include_once('lib/acf/acf.php');
 //define( 'ACF_LITE' , true );
